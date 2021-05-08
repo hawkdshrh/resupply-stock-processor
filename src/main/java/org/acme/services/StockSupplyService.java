293e17925b210b1ef16e90d368de59231589d52e
dhawkins <dhawkins@redhat.com>
2021-05-08 11:29:30 -0400

    Adding logging for debugging

--- src/main/java/org/acme/services/StockSupplyService.java
+++ src/main/java/org/acme/services/StockSupplyService.java
@@ -1,14 +1,12 @@
 package org.acme.services;
 
-import java.util.UUID;
+import io.smallrye.reactive.messaging.kafka.KafkaRecord;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.acme.beans.Product;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import org.acme.beans.SupplyUpdate;
-import org.acme.beans.SupplyUpdateEntry;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
@@ -16,19 +14,15 @@ import org.eclipse.microprofile.reactive.messaging.Emitter;
 public class StockSupplyService {
 
     private static final Logger LOGGER = Logger.getLogger("StockSupplyService");
-
+    
     @Inject
-    @Channel("updated-stock-out")
-    Emitter<SupplyUpdate> emitter;
+    @Channel("stock-levels-out")
+    Emitter<Integer> emitter;
 
     public void updateAvailableStock(String updateId, Product product, Integer amount, String supplyCode) {
 
-        if (updateId == null || updateId.isEmpty()) {
-            updateId = UUID.randomUUID().toString();
-        }
         LOGGER.log(Level.INFO, "Updating sku:{0} for {1} items.", new Object[]{product.getProductSku(), amount});
-        SupplyUpdateEntry entry = new SupplyUpdateEntry(product, amount);
-        SupplyUpdate supplyUpdate = new SupplyUpdate(updateId, supplyCode, new SupplyUpdateEntry[]{entry});
-        emitter.send(supplyUpdate);
+        KafkaRecord<Product, Integer> msg = KafkaRecord.of(product, amount);
+        emitter.send(msg);
     }
 }
